b6189f13561efa28f7bee1954735bd5b372eb293
weslley daeski <weslley.daeski@escola.pr.gov.br>
2026-09-11 09:19:05 -0300

    Update emergencia.js

--- emergencia.js
+++ emergencia.js
@@ -3,10 +3,10 @@ import { getDatabase, ref, onValue, update } from "https://www.gstatic.com/fireb
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js&quot;;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCHxMR6NRrqiNSypsmxywtfu5x9H6ZlvOA",
-    authDomain: "school-tracker-2026.firebaseapp.com",
-    databaseURL: "https://school-tracker-2026-default-rtdb.firebaseio.com&quot;,
-    projectId: "school-tracker-2026"
+    apiKey: "minha API",
+    authDomain: "meu domínio",
+    databaseURL: "meu URL",
+    projectId: "meu ID"
 };
 
 const app = initializeApp(firebaseConfig);
